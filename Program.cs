a68e45eb9504e86e7b8ed2b780f0c6ec72db2371
Kyh <yhoung11@gmail.com>
2024-10-28 15:19:40 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ModBusSlave;
+
+SerialPortConnector serialPortConnector = new SerialPortConnector();
+serialPortConnector.GetPortNames();
+string portName = Console.ReadLine();
+try
+{
+    serialPortConnector.Open(portName);
+}
+catch (Exception e)
+{
+    Console.WriteLine("잘못된 포트");
+    Console.Error.WriteLine(e.Message);
+    return;
+}
+
+Console.WriteLine("연결되었습니다.");
+
+
+
+
+
+
+
+
+
---
+++ SerialPortConnector.cs
@@ -0,0 +1,57 @@
+﻿using System.IO.Ports;
+
+namespace ModBusSlave
+{
+    public class SerialPortConnector
+    {
+        SerialPort seriallPort = new SerialPort();
+
+        public SerialPortConnector()
+        {
+            seriallPort.BaudRate = 115200;
+            seriallPort.Parity = Parity.None;
+            seriallPort.DataBits = 8;
+            seriallPort.StopBits = StopBits.One;
+            seriallPort.Handshake = Handshake.None;
+            seriallPort.ReadTimeout = 500;
+            seriallPort.WriteTimeout = 500;
+        }
+
+        public void Open(string portName)
+        {
+            seriallPort.PortName = portName;
+            seriallPort.Open();
+        }
+
+        public void Close()
+        {
+            seriallPort.Close();
+        }
+
+        public void Write(byte[] data)
+        {
+            seriallPort.Write(data, 0, data.Length);
+        }
+
+        public byte[] Read()
+        {
+            byte[] buffer = new byte[seriallPort.BytesToRead];
+            seriallPort.Read(buffer, 0, buffer.Length);
+            return buffer;
+        }
+
+        public bool IsOpen()
+        {
+            return seriallPort.IsOpen;
+        }
+
+        public string[] GetPortNames()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine("시리얼 포트 선택하기: ");
+            ports.ToList().ForEach(port => Console.WriteLine($"{port} "));
+            return ports;
+        }
+
+    }
+}
